baabba9d926baeb519f4e6c3eb10d90f5b28af9c
Пользователь <@DESKTOP-E7N1RLV>
2023-12-26 19:37:55 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задание 1. Определить, является или нет текущий год високосным? Год ввести с клавиатуры.
+            //Console.Write("введите год:");
+            //int year = int.Parse(Console.ReadLine());
+            //if (year % 4 == 0)
+            //{
+            //    Console.WriteLine($"год высокосный");
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"год не высокосный");
+            //}
+            //Задание 2. Определить, входит и цена выбранного ноутбука в диапазон от 20 до 30 тыс. р.
+            //Console.Write("введите цену :");
+            //int price =int.Parse(Console.ReadLine());
+            //if (price>20000 && price<30000)
+            //{
+            //    Console.WriteLine($"цена выбранного ноутбука входит в диапазон от 20 до 30 тыс. р.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"цена выбранного ноутбука не входит в диапазон от 20 до 30 тыс. р.");
+            //}
+            //Написать программу, которая в зависимости от заданного балла определяет оценку учащегося согласно таблице:
+            //Console.Write("введите ваш балл:");
+            //int point = int.Parse(Console.ReadLine());
+            //if (point >= 0 && point < 60)
+            //{
+            //    Console.WriteLine("неудволетворительно");
+            //}
+            //if (point >= 60 && point <= 74)
+            //{
+            //    Console.WriteLine("удволетворительно");
+            //}
+            //if (point >= 75 && point <= 89)
+            //{
+            //    Console.WriteLine("хорошо");
+            //}
+            //if (point >= 90 && point <= 100)
+            //{
+            //    Console.WriteLine("отлично");
+            //}
+            //if (point >= 0 & point < 60) 
+            //{
+            //    Console.WriteLine("неудволетворительно");
+            //}
+            //else if (point >= 60 && point <= 74)
+            //{
+            //    Console.WriteLine("удволетворительно");
+            //}
+            //else if (point >= 75 && point <= 89)
+            //{
+            //    Console.WriteLine("хорошо");
+            //}
+            //else if (point >= 90 && point <= 100)
+            //{
+            //    Console.WriteLine("отлично");
+            //}
+            //Задание 4. Вычислить значение функции при целых аргументах:
+            //Console.Write("введите значение x:");
+            //int x = int.Parse(Console.ReadLine());
+            //double f =0;
+            //if(x>=0)
+            //{
+            //    f =2*x+Math.Sqrt(x);
+            //}
+            //else
+            //{
+            //    f =Math.Abs(x)+Math.Pow(x,2);  
+            //}
+            //Console.WriteLine(f);
+        }
+    }
+}
